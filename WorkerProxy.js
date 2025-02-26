1bcc52707c842b952c10a2d4887c89766266f396
ywz <ywz@live.com>
2025-02-26 22:51:07 +0800

    Update WorkerProxy.js
    add env

--- WorkerProxy.js
+++ WorkerProxy.js
@@ -2,7 +2,7 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-const myURL = new URL("https://webproxy.stratosphericus.workers.dev/")
+const myURL = new URL(SERVER_URL)
 
 async function handleRequest(request) {
   const url = new URL(request.url)
